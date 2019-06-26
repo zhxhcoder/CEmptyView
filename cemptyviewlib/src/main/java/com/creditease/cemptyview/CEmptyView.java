ab81ecfb936ab48d819422aafdf9c53b670a58fd
zhxh <zhxhcoder@gmail.com>
2019-06-26 14:08:57 +0800

    CEmptyView init

---
+++ cemptyviewlib/src/main/java/com/creditease/cemptyview/CEmptyView.java
@@ -0,0 +1,26 @@
+package com.creditease.cemptyview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by zhxh on 2019/06/26
+ */
+public class CEmptyView extends LinearLayout {
+    public CEmptyView(Context context) {
+        super(context);
+    }
+
+    public CEmptyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CEmptyView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CEmptyView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
